a6199ed221400c5f9a34e1d2667b601b51c1d8aa
HidetoNakasone <highenjoyproject@outlook.jp>
2019-11-18 13:02:40 +0900

    TOPのビュー部分作成。まずは、ユーザーの画像、フォロー・フォロワー関連の情報表示関連だけ上手く作った。

--- app.rb
+++ app.rb
@@ -12,7 +12,51 @@ def client
 end
 
 def login_check
-  redirect '/login' unless session[:user_id].nil?
+  redirect '/login' if session[:user_id].nil?
+end
+
+get '/top' do
+  login_check()
+
+  # 自分についての情報を取得
+  @my_user_id = session[:user_id]
+  @my_user_name = client.exec_params("select user_name from users where id = $1", [@my_user_id]).first['user_name']
+
+  # 自分とフォローしているユーザーの投稿取得
+  @res = client.exec_params("select * from tweets where creater_id = $1 OR (creater_id IN (select send_id from follows where who_id = $1)) ORDER BY id DESC", [@my_user_id])
+
+  # フォロワー情報
+  @res_follower = client.exec_params("select * from users where id IN (select who_id from follows where send_id = $1) ORDER BY id ASC LIMIT 5;", [@my_user_id]).to_a
+
+  my_follow_lists = []
+  client.exec_params('select send_id from follows where who_id = $1', [1]).each { |i| my_follow_lists.push(i['send_id']) }
+
+  @res_follower.each do |i|
+    if my_follow_lists.include?(i['id'])
+      i['is_follow'] = true
+    else
+      i['is_follow'] = false
+    end
+  end
+
+  # フォロー情報
+  @res_follow = client.exec_params("select * from users where id IN (select send_id from follows where who_id = $1) ORDER BY id ASC LIMIT 5;", [@my_user_id])
+
+  # フォロー数
+  @count_follow = client.exec_params("select count(who_id = $1 or null) as n from follows", [@my_user_id]).first['n'].to_i
+
+  # フォロワー数
+  @count_follower = client.exec_params("select count(send_id = $1 or null) as n from follows", [@my_user_id]).first['n'].to_i
+
+  # ツイート数
+  @count_tweet = client.exec_params("select count(creater_id = $1 or null) as n from tweets", [@my_user_id]).first['n'].to_i
+
+  @title = 'TOPページ'
+  @flash = session[:flash]
+  session[:flash] = nil
+  # いいね と リツイート登録 の時はアニメーションをオフにしたいので。
+  @is_animation = session[:is_animation]
+  erb :top
 end
 
 get '/login' do
@@ -91,10 +135,10 @@ post '/signup' do
     '>通知： 入力された名前は、既に使用されています。</p>"
     redirect '/signup'
   else
-    session[:user_id] = client.exec_params('insert into users(user_name, user_pass, user_profile) values($1, $2, $3) returning id;', [name, pass, nil])['id'].to_i
+    session[:user_id] = client.exec_params('insert into users(user_name, user_pass, user_profile) values($1, $2, $3) returning id;', [name, pass, nil]).first['id'].to_i
 
-    FileUtils.mv(params[:new_icon_img][:tempfile], "./public/user_icon/#{session[:user_id]}_icon.jpg")
-    FileUtils.mv(params[:new_back_img][:tempfile], "./public/user_back/#{session[:user_id]}_back.jpg")
+    FileUtils.mv(params[:icon_img][:tempfile], "./public/user_icon/#{session[:user_id]}_icon.jpg")
+    FileUtils.mv(params[:back_img][:tempfile], "./public/user_back/#{session[:user_id]}_back.jpg")
 
     session[:flash] = "<p class='animated fadeInDown' id = 'flash_info' style='
       height: 40px;
@@ -105,7 +149,7 @@ post '/signup' do
       color: white;
       font-size: 1.7em;
       font-weight: solid;
-    '>成功： 登録が無事処理されました。続けてログインして下さい。</p>"
+    '>成功： 登録が無事処理されました。ようこそ。</p>"
     redirect '/login'
   end
 end
